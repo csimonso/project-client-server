19f21868f6b4798a9c7f826eb750cabf4319ce45
Yosua Oey <yoey@ucsd.edu>
2017-09-01 14:26:29 -0700

    Implementing Write

--- Client/client.c
+++ Client/client.c
@@ -29,15 +29,11 @@ int main(int argc , char *argv[]) {
 	// Copy Internet address that was retrieved via gethostnameby() to serv_addr struct
 	memcpy( (void *) &serv_addr.sin_addr, hp->h_addr_list[0], hp->h_length );
 
-	memset( (char *) &myaddr, 0, sizeof(myaddr) );
-	myaddr.sin_family		= AF_INET;
-	myaddr.sin_port 		= htons(PORT_NUMBER);
-	myaddr.sin_addr.s_addr  = htonl(INADDR_ANY);
-
-	/* Send Request to Server */
-	// Connect socket to address specified by the second parameter (Server?)
-	int connect_status = connect( fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr) );
-	if( connect_status == -1 ){ perror("Error on connecting\n"); }
+	/* Maybe not necessary (changed from myaddr.something to serv_addr.something) */
+	/// memset( (char *) &myaddr, 0, sizeof(myaddr) );
+	serv_addr.sin_family		= AF_INET;
+	serv_addr.sin_port 			= htons(PORT_NUMBER);
+	serv_addr.sin_addr.s_addr  	= htonl(INADDR_ANY);
 
 	// More variable declarations
 	FILE *output = NULL;
@@ -49,75 +45,100 @@ int main(int argc , char *argv[]) {
 	int block_count = 0;
 	int byte_count = 0;	// Not sure if this is necessary.
 
-	// Request the file from the server
-	buffer[0] = 0;
-	buffer[1] = 1;
-	strcpy( &buffer[2], file_name );
-	strcpy( &buffer[2 + strlen(file_name) + 1], "octet" );
+	if( argv[1] == "-r" ) {
+		/* Send Request to Server */
+		// Connect socket to address specified by the second parameter (Server?)
+		// May not be necessary
+		/* int connect_status = connect( fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr) );
+		if( connect_status == -1 ){ perror("Error on connecting\n"); } */
 
-	// char TEMP_HEADER[1000] = "TODO: Make the request header for RRQ (Read Request) & WRQ (Write Request)"
-	// TODO: Timer
-	size_t send_status = sendto( fd, buffer, DATA_LENGTH + 4, 0,
-		(struct sockaddr *) &serv_addr, sizeof(serv_addr) );
-	if( send_status == -1 ) { perror("Request failed\n"); }
-
-	/* Receive data after request from server */
-	while(1)
-	{
-		// Fetch server's response, and stores the length of the message (data) to variable recv_count.
-		recv_count = recvfrom( fd, buffer, DATA_LENGTH + 4, 0,
-			(struct sockaddr *) &serv_addr, sizeof(serv_addr) );
-		if( recv_count == -1 ) { perror("Receive failed!"); }
-
-	   /* Not sure if this is necessary.
-		* // Make sure it's data
-		* op_code = (buffer[0] << 8) | buffer[1];
-	    *     if( op_code == 5 ) {
-	    *         printf( "Error from server: %s\n", &buffer[4] );
-	    *         break;  // Do we really want to do this?
-	    *     }
-		*/
-
-		// Acquire Block Number of Data. Block number is to distinguish between duplicate data.
-		// TODO: Figure this out.
-		block_number = (buffer[2] << 8) | (buffer[3] & 0x00FF);
-
-		// Strip paths off file name. Be sure the output file is open.
-	    if( output == NULL ) {
-	        // TODO: This doesn't handle trailing slash characters very well but presumably
-	        //       they would cause errors anyway.
-	        simple_file_name = strrchr( file_name, '/' );
-	        if( simple_file_name == NULL )
-	            simple_file_name = file_name;
-	        else
-	            simple_file_name = simple_file_name + 1;
-
-	        output = fopen( simple_file_name, "w" );
-	        if( output == NULL ) {
-	            printf( "Unable to open %s (after receiving block #%u)", simple_file_name, block_number );
-	        }
-		}
-
-		// If there's any message, continually writes to "output" while keeping track of block_count & byte_count.
-		if( (recv_count > 4) && (block_number == block_count + 1) )
-		{
-            block_count++;
-            byte_count += (recv_count - 4);
-            fwrite( &buffer[4], 1, recv_count - 4, output );
-		}
-
-		/* Send ACK/NAK to server */
-		// Set ACK OpCode
+		// Request the file from the server
 		buffer[0] = 0;
-		buffer[4] = 4;
+		buffer[1] = 1;
+		strcpy( &buffer[2], file_name );
+		strcpy( &buffer[2 + strlen(file_name) + 1], "octet" );
 
-		// Send ACK to indicate client received the file.
-		send_status = sendto( fd, buffer, ACK_LENGTH, 0,
-			(const struct sockaddr *) &serv_addr, sizeof(serv_addr) );
-		if (send_status == -1)  { perror("Acknowledgement failure"); }
+		// TODO: Timer
+		// Send RRQ to server
+		size_t send_status = sendto( fd, buffer, DATA_LENGTH + 4, 0,
+			(struct sockaddr *) &serv_addr, sizeof(serv_addr) );
+		if( send_status == -1 ) { perror("Read Request failed\n"); }
 
-		// Checks if this is the last packet. If so, break out of the while loop.
-		if( recv_count - 4 < 512 )  break;
+		/* Receive data after request from server */
+		while(1)
+		{
+			// Fetch server's response, and stores the length of the message (data) to variable recv_count.
+			recv_count = recvfrom( fd, buffer, DATA_LENGTH + 4, 0,
+				(struct sockaddr *) &serv_addr, sizeof(serv_addr) );
+			if( recv_count == -1 ) { perror("Receive failed!"); }
+
+		   /* Not sure if this is necessary.
+			* // Make sure it's data
+			* op_code = (buffer[0] << 8) | buffer[1];
+		    *     if( op_code == 5 ) {
+		    *         printf( "Error from server: %s\n", &buffer[4] );
+		    *         break;  // Do we really want to do this?
+		    *     }
+			*/
+
+			// Acquire Block Number of Data. Block number is to distinguish between duplicate data.
+			// TODO: Figure this out.
+			block_number = (buffer[2] << 8) | (buffer[3] & 0x00FF);
+
+			// Strip paths off file name. Be sure the output file is open.
+		    if( output == NULL ) {
+		        // TODO: This doesn't handle trailing slash characters very well but presumably
+		        //       they would cause errors anyway.
+		        simple_file_name = strrchr( file_name, '/' );
+		        if( simple_file_name == NULL )
+		            simple_file_name = file_name;
+		        else
+		            simple_file_name = simple_file_name + 1;
+
+		        output = fopen( simple_file_name, "w" );
+		        if( output == NULL ) {
+		            printf( "Unable to open %s (after receiving block #%u)", simple_file_name, block_number );
+		        }
+			}
+
+			// If there's any message, continually writes to "output" while keeping track of block_count & byte_count.
+			if( (recv_count > 4) && (block_number == block_count + 1) )
+			{
+	            block_count++;
+	            byte_count += (recv_count - 4);
+	            fwrite( &buffer[4], 1, recv_count - 4, output );
+			}
+
+			/* Send ACK/NAK to server */
+			// Set ACK OpCode
+			buffer[0] = 0;
+			buffer[1] = 4;
+
+			// Send ACK to indicate client received the file.
+			send_status = sendto( fd, buffer, ACK_LENGTH, 0,
+				(const struct sockaddr *) &serv_addr, sizeof(serv_addr) );
+			if (send_status == -1)  { perror("Acknowledgement failure"); }
+
+			// Checks if this is the last packet. If so, break out of the while loop.
+			if( recv_count - 4 < 512 )  break;
+		}
+	} else if ( argv[1] == "-w" ) {
+		// Initiate header for write
+		buffer[0] = 0;
+		buffer[1] = 2;
+		strcpy( &buffer[2], file_name );
+		strcpy( &buffer[2 + strlen(file_name) + 1], "octet" );
+
+		// Send Write 
+		size_t send_status = sendto( fd, buffer, DATA_LENGTH + 4, 0,
+			(struct sockaddr *) &serv_addr, sizeof(serv_addr) );
+		if( send_status == -1 ) { perror("Write Request failed\n"); }
+
+		// 
+		block_number = (buffer[2] << 8) | (buffer[3] & 0x00FF);
+	} else {
+		// If option is invalid (i.e. -t), print out Invalid Option with list of valid options.
+		printf("\nInvalid Option. Type -r to read file or -w to write file\n");
 	}
 
 	// Closing the output file.
